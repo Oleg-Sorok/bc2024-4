5f79c80727f25b7efadedd6ed4da8949165d5aa5
Oleg-Sorok bc2024-4 <olegsorokevych@gmail.com>
2024-10-23 11:23:23 +0300

    created http methods

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const { program } = require('commander');
+const fs = require('fs').promises;
+const path = require('path');
 
 // Налаштування командного рядка
 program
@@ -11,10 +13,58 @@ program
 const { host, port, cache } = program.opts();
 
 // Створення вебсервера
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!\n');
+const server = http.createServer(async (req, res) => {
+  const code = req.url.slice(1); // Отримуємо код HTTP з URL (без '/')
+  const filePath = path.join(".cache", `${code}.jpg`);
+
+  if (req.method === 'GET') {
+    try {
+      const fileData = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(fileData);
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server error');
+      }
+    }
+  } else if (req.method === 'PUT') {
+    let data = [];
+    req.on('data', chunk => {
+      data.push(chunk);
+    });
+    req.on('end', async () => {
+      const buffer = Buffer.concat(data);
+      try {
+        await fs.writeFile(filePath, buffer);
+        res.writeHead(201, { 'Content-Type': 'text/plain' });
+        res.end('File created/updated');
+      } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server error');
+      }
+    });
+  } else if (req.method === 'DELETE') {
+    try {
+      await fs.unlink(filePath);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('File deleted');
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server error');
+      }
+    }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method not allowed');
+  }
 });
 
 server.listen(port, host, () => {
